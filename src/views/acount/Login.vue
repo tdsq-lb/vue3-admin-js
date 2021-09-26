9d6258629df05905dc31d7401b4b98e8bd77e118
libiao <578556500@qq.com>
2021-09-26 09:48:04 +0800

    1

---
+++ src/views/acount/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Login_container">
+    <a-button type="primary">Primary</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
